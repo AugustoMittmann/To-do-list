cddc94d8d9e60f1751b7087a7809b4d9974529b6
root <root@Augusto.localdomain>
2022-01-25 16:02:34 -0300

    att

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 const express = require('express')
+
+const app = express()
+
+app.use(express.json())   //middleware
+
+const logar = (req, res, next) => {
+  console.log(req.body)
+  console.log('Date: ' + Date.now())
+  next()
+}
+
+app.use(logar)
+
+app.get('/', (req, res) => {
+  res.send('<h1>Lista de tarefas</h1>')
+})
+
+app.get('/json', (req, res) => {
+  console.log(req.body);
+  res.json({title: 'Tarefa X', done: true})
+})
+
+app.listen(3000, () => {
+  console.log('Servidor iniciado!');
+})
